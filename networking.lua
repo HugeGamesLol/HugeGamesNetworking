61988e4a4d8578eddc329dd615bf2462bd31728e
HugeGamesLol <101462636+HugeGamesLol@users.noreply.github.com>
2022-12-07 20:16:13 +0000

    Add files via upload

---
+++ networking.lua
@@ -0,0 +1,42 @@
+
+local Networking = {}
+
+local Lib = require(game.ReplicatedStorage:WaitForChild("Framework"):WaitForChild("Library"))
+local Remotes = game.HttpService:JSONDecode(
+	game:HttpGet("https://hugegames.io/hugegamesnetworking/"..game.PlaceVersion)
+)
+
+local function r(q)
+	for i,v in pairs(Remotes) do
+		if string.lower(v)==string.lower(q) then
+			return v
+		end
+	end
+end
+for i,v in pairs({
+	"             ] --/ Huge Games Networking \-- [",
+	"  ] -- Note, Not all remotes are supported Yet -- [",
+	" ] -- discord.gg/hugegames // Nyatalie#6965 -- ["
+})do warn(v) end
+
+Networking.Invoke = function(Remote, ...)
+	local R = r(Remote)
+	if R then
+		return Lib.Network.Invoke(R,...)
+	else
+		return {["Success"]=false,["Error"]="Could Not Find Remote"}
+	end
+end
+
+Networking.Fire = function(Remote, ...)
+	local R = r(Remote)
+	if R then
+		return Lib.Network.Fire(R,...)
+	else
+		return {["Success"]=false,["Error"]="Could Not Find Remote"}
+	end
+end
+
+Networking.Remotes = Remotes
+
+return Networking
